b79cc2edb1f63fd9891cc821bd5e835608ccf885
Matt Dainty <matt@bodgit-n-scarper.com>
2020-11-21 22:40:40 +0000

    Rubocop fixes

--- spec/acceptance/0_portmap_spec.rb
+++ spec/acceptance/0_portmap_spec.rb
@@ -4,51 +4,51 @@ describe 'portmap' do
   case fact('osfamily')
   when 'RedHat'
     package_name = 'rpcbind'
-    case fact('operatingsystemmajrelease')
-    when '6'
-      service_name = 'rpcbind'
-    else
-      service_name = 'rpcbind.socket'
-    end
+    service_name = case fact('operatingsystemmajrelease')
+                   when '6'
+                     'rpcbind'
+                   else
+                     'rpcbind.socket'
+                   end
   when 'Debian'
     package_name = 'rpcbind'
-    case fact('operatingsystem')
-    when 'Ubuntu'
-      case fact('operatingsystemrelease')
-      when '14.04'
-        service_name = 'rpcbind'
-      else
-        service_name = 'rpcbind.socket'
-      end
-    else
-      service_name = 'rpcbind'
-    end
+    service_name = case fact('operatingsystem')
+                   when 'Ubuntu'
+                     case fact('operatingsystemrelease')
+                     when '14.04'
+                       'rpcbind'
+                     else
+                       'rpcbind.socket'
+                     end
+                   else
+                     'rpcbind'
+                   end
   when 'OpenBSD'
     service_name = 'portmap'
   end
 
-  it 'should work with no errors' do
+  it 'works with no errors' do
     pp = <<-EOS
       include ::portmap
     EOS
 
-    apply_manifest(pp, :catch_failures => true)
-    apply_manifest(pp, :catch_changes  => true)
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes:  true)
   end
 
-  describe package(package_name), :unless => fact('osfamily').eql?('OpenBSD') do
-    it { should be_installed }
+  describe package(package_name), unless: fact('osfamily').eql?('OpenBSD') do
+    it { is_expected.to be_installed }
   end
 
   describe service(service_name) do
-    it { should be_enabled }
-    it { should be_running }
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
   end
 
   # Explicitly adding the '-p' changes the output on the rpcbind version
   describe command('rpcinfo -p') do
-    its(:stdout) { should match /100000 \s+ 2 \s+ tcp \s+ 111 \s+ portmapper/x }
-    its(:stdout) { should match /100000 \s+ 2 \s+ udp \s+ 111 \s+ portmapper/x }
-    its(:exit_status) { should eq 0 }
+    its(:stdout) { is_expected.to match %r{100000 \s+ 2 \s+ tcp \s+ 111 \s+ portmapper}x }
+    its(:stdout) { is_expected.to match %r{100000 \s+ 2 \s+ udp \s+ 111 \s+ portmapper}x }
+    its(:exit_status) { is_expected.to eq 0 }
   end
 end
